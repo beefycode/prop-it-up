fabc75f9d2170b3724c186c70948d35b42fc792d
beefycode <koru.louis@gmail.com>
2023-01-14 13:01:53 +1300

    final code

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import './components/PersonCard';
+import PersonCard from './components/PersonCard';
 
 function App() {
   return (
-    
+    <div className='App'>
+      <PersonCard
+        firstName={"Abe"}
+        lastName={"Lincoln"}
+        age={99}
+        hairColor={"Gray"}/>
+      <PersonCard
+        firstName={"Frank"}
+        lastName={"Harper"}
+        age={12}
+        hairColor={"Black"}/>
+      <PersonCard
+        firstName={"Mandy"}
+        lastName={"Mulligans"}
+        age={24}
+        hairColor={"Blonde"}/>
+</div>
   );
 }
 
